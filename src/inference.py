73a910c0254f63f4773396fe2219d8fec4f68a7f
tuckerdickson <nathantuckerdickson@gmail.com>
2024-01-15 16:02:44 -0500

    add requirements.txt.

--- src/inference.py
+++ src/inference.py
@@ -1,5 +1,16 @@
-def main():
-    print("this is inference.py")
+import sys
+import pandas as pd
+
+
+def main(input_path, output_path):
+    df = read_df(input_path)
+    df.to_csv(output_path, index=False)
+
+
+def read_df(file_path):
+    df = pd.read_csv(file_path)
+    return df
+
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1], sys.argv[2])
